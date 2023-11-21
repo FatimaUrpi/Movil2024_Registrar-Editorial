de9b6d2b4559dc93a3f2cd3d3a1e2efffd2f6e1a
jhanio <jhaniohernandez@outlook.com>
2023-11-20 23:25:26 -0500

    PC-3 Desarrollo service editorial

--- app/src/main/java/com/cibertec/proyecto/service/ServiceEditorial.java
+++ app/src/main/java/com/cibertec/proyecto/service/ServiceEditorial.java
@@ -1,5 +1,6 @@
 package com.cibertec.proyecto.service;
 
+import com.cibertec.proyecto.entity.Autor;
 import com.cibertec.proyecto.entity.Editorial;
 import com.cibertec.proyecto.entity.Sala;
 
@@ -9,13 +10,23 @@ import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
 import retrofit2.http.Path;
 
 public interface ServiceEditorial {
 
+    @GET("editorial/porNumero/{numero}")
+    public abstract Call<List<Editorial>> listaEditorial(@Path("numero") String numero);
+
+    @GET("editorial")
+    public abstract Call<List<Editorial>> listaEditorial();
     @GET("editorial/porNombre/{nombre}")
-    public Call<List<Editorial>> listaporEditorial(@Path("nombre") String nombre);
+    public abstract Call<List<Editorial>> listaXEditorial(@Path("nombre")String nombre);
+
     @POST("editorial")
     public abstract Call<Editorial> insertaEditorial(@Body Editorial objEditorial);
-    ;
+
+    @PUT("editorial")
+    public abstract Call<Editorial> actualizar(@Body Editorial objEditorial);
+
 }
